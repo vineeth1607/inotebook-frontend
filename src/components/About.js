e09b2886cd7c6b0a62acb701aafb61c410be7200
vineeth1607 <vineethayaan@gmail.com>
2024-03-26 19:23:56 +0530

    created components
    created Navbar, Home and about Components
    
    created Router setup in the app.js and configured navbar, home and about

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => {
+  return (
+<h2>This is About</h2>
+  )
+}
+
+export default About
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <h1>iNotebook</h1>
+        </div>
+    )
+}
+
+export default Home
